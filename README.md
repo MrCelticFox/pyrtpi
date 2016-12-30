e029e3af6f0da2af902f97e3c94091d62d2585f4
MrCelticFox <diarmaid@mrcelticfox.com>
2016-12-30 17:31:07 +0000

    Added option to limit results to one route

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ A command line interface for Dublin Bus' Real Time Passenger Information, writte
 
 ## Usage
 The script is run like so:  
-`python rtpi.py <stop_number>`  
+`python rtpi.py <stop_number> [<bus_number>]`  
 where `<stop_number>` is the number of the bus stop that you want to get the times for.
+An optional parameter `<bus_number>` may also be specified which will limit the results
+to only display times for that route.
 
 ## Compatibility
 This project is designed to be run with Python 3 or above. Versions below that are not supported and are not guaranteed to work.
--- rtpi.py
+++ rtpi.py
@@ -1,13 +1,13 @@
-# Author: Diarmaid Clancy, <diarmaid@mrcelticfox.com>
 from lxml import html
 import requests
 import sys
 
 if (len(sys.argv) < 2):
-    print("Usage: python rtpi.py <stop_number>")
+    print("Usage: python rtpi.py <stop_number> [<bus_number>]")
     exit(0)
 
 stop_number = sys.argv[1]
+bus_number = sys.argv[2] if (len(sys.argv) == 3) else False
 link ='http://www.dublinbus.ie/RTPI/Sources-of-Real-Time-Information/?searchtype=view&searchquery='
 
 page = requests.get(link+stop_number)
@@ -22,7 +22,10 @@ headings = '{:<8}{:<50}{:<8}\n'.format('Route', 'Destination', 'Expected time')
 print(headings)
 
 i = 0
-while(i<len(times)):
+while (i<len(times)):
+    if (bus_number and (bus_number != times[i])):
+        i += 3
+        continue
     row = '{:<8}{:<50}{:<8}'.format(times[i],times[i+1],times[i+2])
     print(row)
     i += 3
