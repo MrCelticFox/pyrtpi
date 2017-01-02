7a10179aa37850cf53ddeba4e855d39220fbe19e
MrCelticFox <diarmaid@mrcelticfox.com>
2017-01-02 17:51:26 +0000

    Added more flexible options (see README for details)

--- README.md
+++ README.md
@@ -3,11 +3,15 @@
 A command line interface for Dublin Bus' Real Time Passenger Information, written in Python.
 
 ## Usage
-The script is run like so:  
-`python rtpi.py <stop_number> [<bus_number>]`  
-where `<stop_number>` is the number of the bus stop that you want to get the times for.
-An optional parameter `<bus_number>` may also be specified which will limit the results
-to only display times for that route.
+The script is run like so: `python rtpi.py <stop_number> [OPTIONS]`
+where `<stop_number>` is the number of the bus stop that you want to get the times for.  
+Currently supported options are as follows:
+- `-n <num>` : allows the user to limit the number of results displayed
+- `-b <bnum>` : allows the user to only display results for a certain bus route
+- `-h` : prints help information and exits
+
+Note that options can be combined, so running `python rtpi.py 192 -b 46a -n 3`
+will display the first 3 times for the 46a bus at stop 192.
 
 ## Compatibility
 This project is designed to be run with Python 3 or above. Versions below that are not supported and are not guaranteed to work.
--- rtpi.py
+++ rtpi.py
@@ -1,31 +1,59 @@
 from lxml import html
 import requests
 import sys
+from getopt import getopt
+
+def printHelp():
+    print("Usage: python rtpi.py <stop_number> [OPTIONS]\n")
+    print("-n <num>\tnumber of results to print")
+    print("-b <bnum>\tspecify a bus number to limit results to a specific route")
+    print("-h\tprint this help message and exit")
+    exit(0)
 
 if (len(sys.argv) < 2):
-    print("Usage: python rtpi.py <stop_number> [<bus_number>]")
+    printHelp()
     exit(0)
 
 stop_number = sys.argv[1]
-bus_number = sys.argv[2] if (len(sys.argv) == 3) else False
-link ='http://www.dublinbus.ie/RTPI/Sources-of-Real-Time-Information/?searchtype=view&searchquery='
+bus_number = ''
+n_results = 0
 
+# handle options
+options = getopt(sys.argv[2:], "n:b:h")[0]
+for option in options:
+    if option[0] == '-n':
+        n_results = int(option[1])
+    elif option[0] == '-b':
+        bus_number = option[1]
+    elif option[0] == '-h':
+        printHelp()
+
+# retrieve information from Dublin bus website
+link ='http://www.dublinbus.ie/RTPI/Sources-of-Real-Time-Information/?searchtype=view&searchquery='
 page = requests.get(link+stop_number)
 tree = html.fromstring(page.content)
-
 path = '//tr[@class="odd"]/td[position()<4]/text() | ' + \
         '//tr[@class="even"]/td[position()<4]/text()'
 dirty = tree.xpath(path)
 times = [s.strip() for s in dirty]
+
+if (n_results == 0): # if no limit set then show all results
+    n_results = len(times)/3
 
 headings = '{:<8}{:<50}{:<8}\n'.format('Route', 'Destination', 'Expected time')
 print(headings)
 
 i = 0
 while (i<len(times)):
-    if (bus_number and (bus_number != times[i])):
+    # if set, show only results for the specified route
+    if (bus_number != '' and bus_number != times[i]):
         i += 3
         continue
+
+    if (n_results == 0):
+        break
+
     row = '{:<8}{:<50}{:<8}'.format(times[i],times[i+1],times[i+2])
     print(row)
+    n_results -= 1
     i += 3
